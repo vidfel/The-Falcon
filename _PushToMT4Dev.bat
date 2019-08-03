0e3f839756e51a437ae8602a9c50ddccdf7b3fe6
vidfel <vidal.luisfelipe@gmail.com>
2019-08-03 05:11:52 +0000

    Update with .bat Files

--- _PushToMT4Dev.bat
+++ _PushToMT4Dev.bat
@@ -5,8 +5,8 @@ rem Use in case some content needs to be replaced (reverted from Version Control
 setlocal enabledelayedexpansion
 
 :: Source Directory where Version Control Repository is located
-set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\The-Falcon"
+set SOURCE_DIR="C:\Users\vidfel\Documents\GitHub\The-Falcon"
 :: Destination Directory where Expert Advisor is located
-set DEST_DIR="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts\02_The-Falcon"
+set DEST_DIR="C:\Program Files (x86)\OANDA - MetaTrader - Terminal2\MQL4\Experts\02_The-Falcon"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR% *.mq4
--- _PushToMT4Prod.bat
+++ _PushToMT4Prod.bat
@@ -4,11 +4,11 @@ rem Use when you need to publish all files to All Terminals
 @echo off
 setlocal enabledelayedexpansion
 
-set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\The-Falcon"
-set DEST_DIR1="C:\Program Files (x86)\FxPro - Terminal1\MQL4\Experts\02_The-Falcon"
-set DEST_DIR2="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts\02_The-Falcon"
-set DEST_DIR3="C:\Program Files (x86)\FxPro - Terminal3\MQL4\Experts\02_The-Falcon"
-set DEST_DIR4="C:\Program Files (x86)\FxPro - Terminal4\MQL4\Experts\02_The-Falcon"
+set SOURCE_DIR="C:\Users\vidfel\Documents\GitHub\The-Falcon"
+set DEST_DIR1="C:\Program Files (x86)\OANDA - MetaTrader - Terminal1\MQL4\Experts\02_The-Falcon"
+set DEST_DIR2="C:\Program Files (x86)\OANDA - MetaTrader - Terminal2\MQL4\Experts\02_The-Falcon"
+set DEST_DIR3="C:\Program Files (x86)\OANDA - MetaTrader - Terminal3\MQL4\Experts\02_The-Falcon"
+set DEST_DIR4="C:\Program Files (x86)\OANDA - MetaTrader - Terminal4\MQL4\Experts\02_The-Falcon"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR1% *.mq4
 ROBOCOPY %SOURCE_DIR% %DEST_DIR2% *.mq4
